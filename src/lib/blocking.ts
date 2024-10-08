3d22872c618b1b134a8269dce349c5e5172ceeca
Dima Lipin <dimich3d@ya.ru>
2024-10-08 21:00:07 +0200

    fmt

--- src/lib/blocking.ts
+++ src/lib/blocking.ts
@@ -9,11 +9,11 @@ interface BlockOptions {
 export async function blockPage(options: BlockOptions) {
   const focus = await browser.storage.local.get('focus');
   const whitelist = await browser.storage.local.get('whitelist');
-  
+
   if (focus.focus === true) {
     // Check if the URL is in the whitelist
     const isWhitelisted = whitelist.whitelist && whitelist.whitelist.some((site: string) => options.url.includes(site));
-    
+
     if (!isWhitelisted) {
       console.log('blocked page', options);
       const redirectProps = { url: 'https://todoist.com' };
