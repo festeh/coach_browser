211b4e56ee64f53e5311cb0d7e244802f06546a3
Dmitrii Lipin <dimich3d@ya.ru>
2025-03-18 21:22:08 +0100

    icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  outDir: 'dist',
   manifest: {
     icons: {
-      16: "/icon16.jpeg",
-      32: "/icon32.jpeg",
-      48: "/icon48.jpeg",
-      128: "/icon128.jpeg",
+      16: "/c-16.jpeg",
+      32: "/c-32.jpeg",
+      48: "/c-128.jpeg",
+      128: "/c-128.jpeg",
     },
     permissions: ["notifications", "tabs", "storage", "webNavigation"],
     browser_specific_settings: {
